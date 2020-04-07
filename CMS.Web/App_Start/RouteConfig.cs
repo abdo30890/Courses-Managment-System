3b446191aa49b332134ace1987d2294274f49deb
adbdelrahman ahmed <abdo.30890@gmail.com>
2020-04-07 20:48:18 +0200

    Creating areas

--- CMS.Web/App_Start/RouteConfig.cs
+++ CMS.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace CMS.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional },
+                new string[] {"CMS.Web.Controllers"}
             );
         }
     }
---
+++ CMS.Web/Areas/Admin/Controllers/CourseController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CMS.Web.Models;
+
+namespace CMS.Web.Areas.Admin.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Admin/Course
+        public ActionResult Index()
+        {
+            var courses = new List<Course>
+            {
+                new Course
+                {
+                    Id = 1,
+                    CourseName = "Programming",
+                    Description = "Welcome to new project",
+
+
+                }
+
+            };
+            return View(courses);
+        }
+    }
+}
